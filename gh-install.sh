15b1f6b42bd9e9e4bfcbadba8868ff4d33e913ca
Chuck Boecking <chuck@chuboe.com>
2026-04-02 00:32:57 +0000

    added gh installer - v3

--- gh-install.sh
+++ gh-install.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
-#https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+echo
+echo 'you have two options - uncomment the one you want'
+echo
 
-(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+echo '=== Option 1 ==='
+echo 'https://github.com/cli/cli/blob/trunk/docs/install_linux.md'
+echo '  -> uncomment in file'
+echo '=== Option 1 ==='
+echo
 
-sudo apt update
-sudo apt install gh
+#(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+#	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+#	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+#	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+#	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+#	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+#	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+#	&& sudo apt update \
+#	&& sudo apt install gh -y
+#
+#sudo apt update
+#sudo apt install gh
 
-#Run `gh auth login`
+echo '=== Option 2 ==='
+echo 'nix profile add nixpkgs#gh'
+echo '=== Option 2 ==='
+echo
+
+# nix alternative
+# nix profile add nixpkgs#gh
+
+echo '=== Authenticate ==='
+echo 'Run `gh auth login`'
+echo '=== Authenticate ==='
+echo
